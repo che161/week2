0d714b05c7e9b15037b0318286e1888202eed020
Chen <che161@csiro.au>
2019-09-25 15:48:31 +1000

    week2 1

---
+++ src/week2.R
@@ -0,0 +1,13 @@
+#installed.package("tidyverse")
+library(tidyverse)
+read_csv("data/gapminder.csv")
+gapminder <- read_csv("data/gapminder.csv")
+nrow(gapminder)
+ncol(gapminder)
+colnames(gapminder)
+glimpse(gapminder)
+ncol(storms)
+nrow(storms)
+glimpse(storms)
+storms
+d
