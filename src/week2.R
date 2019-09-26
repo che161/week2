febc184f7e56c7da13aa6f30805348d7dc910a55
Chen <che161@csiro.au>
2019-09-26 14:34:18 +1000

    week2 challenges

--- src/week2.R
+++ src/week2.R
@@ -2,48 +2,78 @@
 library(tidyverse)
 read_csv("data/BOM_data.csv")
 BOM <- read_csv("data/BOM_data.csv")
-BOM %>% 
-  filter(Temp_min_max > 0., Rainfall > 0.) %>% 
-  group_by(Station_number) %>% 
-  summarise(num_rows = n())
 
-
-BOM2 <- BOM %>% 
-  filter(Temp_min_max > 0., Rainfall > 0.) %>% 
-  group_by(Station_number) %>% 
-  separate(col=Temp_min_max, into = c("Temp_min", "Temp_max"), sep = "/") %>% 
-  filter(Temp_min > 0., Temp_max > 0.)
-BOM2 %>%  mutate(Temp_diff = Temp_max - Temp_min )
-
-
+#read_csv("data/BOM_stations.csv",col_names = FALSE)
 read_csv("data/BOM_stations.csv")
-BOM_Stations <- read_csv("data/BOM_stations.csv")
-?filter
-write.csv("res/By_Station.csv")
-?mutate
-
-#Challenge 1
-BOM %>% 
-  filter(Temp_min_max > -99., Rainfall >= 0.) %>% 
-  group_by(Station_number) %>% 
-  separate(col=Temp_min_max, into = c("Temp_min", "Temp_max"), sep = "/") %>% 
-  filter(Temp_min > -99., Temp_max > -99.) %>%   
-  summarise(num_rows = n())
 
 #Questsion 1
 BOM %>% 
   separate(col=Temp_min_max, into = c("Temp_min", "Temp_max"), sep = "/") %>% 
   filter(Temp_min != "-", Temp_max != "-", Rainfall >= 0.) %>% 
   group_by(Station_number) %>% 
-  summarise(num_rows = n())
+  summarise(num_rows = n()) %>% 
+  write.csv("res/Question1.csv")
 
 #Questsion 2
 BOM %>% 
   separate(col=Temp_min_max, into = c("Temp_min", "Temp_max"), sep = "/") %>% 
   filter(Temp_min != "-", Temp_max != "-", Rainfall >= 0.) %>% 
   mutate(Temp_diff = as.numeric(Temp_max) - as.numeric(Temp_min)) %>% 
-  group_by(Station_number,Month) %>% 
+  group_by(Month) %>% 
   summarise(mean_tem_diff = mean(Temp_diff)) %>% 
-  filter(mean_tem_diff == min(mean_tem_diff))  
+  filter(mean_tem_diff == min(mean_tem_diff)) %>% 
+  write.csv("res/Question2.csv")
 
 #Questsion 3
+#BOM_Stations <- read_csv("data/BOM_stations.csv",col_names = FALSE)
+BOM_Stations <- read_csv("data/BOM_stations.csv")
+BOM_Stations
+gather(BOM_Stations, Station_number, val, -info) %>% 
+  spread(info, val) %>% 
+    select(Station_number, state)
+#BOM_St_Tidy <- 
+MinTemp <- BOM %>% 
+    separate(col=Temp_min_max, into = c("Temp_min", "Temp_max"), sep = "/") %>% 
+    filter(Temp_min != "-", Temp_max != "-", Rainfall >= 0.) %>% 
+    mutate(Temp_diff = as.numeric(Temp_max) - as.numeric(Temp_min)) %>% 
+    group_by(Station_number) %>% 
+    summarise(mean_tem_diff = mean(Temp_diff))  
+MinTemp 
+ 
+BOM_Stations_Tidy <- gather(BOM_Stations, Station_number, val, -info) %>% 
+  spread(info, val) %>% 
+  select(Station_number, state)
+BOM_Stations_Tidy
+# change station number from string to numeric
+BOM_Stations_Tidy2 <-   mutate(BOM_Stations_Tidy, station = as.numeric(Station_number)) %>% 
+    select(station,state) %>% 
+  rename(Station_number = station)
+
+inner_join(MinTemp,BOM_Stations_Tidy2) %>% 
+  filter(mean_tem_diff == min(mean_tem_diff))  %>% 
+  write.csv("res/Question3.csv")
+
+#Questsion 4
+BOM_Stations <- read_csv("data/BOM_stations.csv")
+BOM_Stations
+Station_Lon <-   gather(BOM_Stations, Station_number, val, -info) %>% 
+  spread(info, val) %>% 
+  select(Station_number, lon)
+# change station number from string to numeric
+Station_Lon2 <-   mutate(Station_Lon, station = as.numeric(Station_number)) %>% 
+  select(station,lon) %>% 
+  rename(Station_number = station)
+Station_Lon2
+
+#work out mean solar
+MeanSol <- BOM %>% 
+  separate(col=Temp_min_max, into = c("Temp_min", "Temp_max"), sep = "/") %>% 
+  filter(Solar_exposure != "-") %>% 
+  mutate(Solar = as.numeric(Solar_exposure)) %>% 
+  group_by(Station_number) %>% 
+  summarise(mean_solar = mean(Solar))  
+MeanSol 
+
+inner_join(MeanSol,Station_Lon2) %>% 
+  filter(lon == min(lon) | lon == max(lon)) %>% 
+  write.csv("res/Question4.csv")
